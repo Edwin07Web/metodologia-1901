74c25f14c59b9a2e6422114a5887e1f61372fed2
T-107 <T-107@PC186>
2018-10-01 17:11:41 -0500

    IMC DE UNA PERSONA

---
+++ metodologia-1901/src/Ej1/Probar.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ej1;
+
+/**
+ *
+ * @author T-107 
+ */
+public class Probar {
+    public static void main(String[] args) {
+        
+        
+        Persona p=new Persona();
+        
+        p.peso=80;
+        p.altura=1.71f;
+        
+        
+        
+        Imc i=new Imc();
+        
+        i.p=p;
+        i.calcular();
+        
+        System.out.println("El IMC de la persona es :"+i.valor);
+        
+        
+    }
+}
+
